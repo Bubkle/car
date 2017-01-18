d025eb681c3fc784e3fa5370af0cffb9670560e9
bubkle <bubkle@163.com>
2017-01-18 11:51:51 +0800

    增加了几个表单字段

--- app/car/forms.py
+++ app/car/forms.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SubmitField, DateField, FloatField, FileField
+from wtforms import StringField, SelectField, SubmitField, DateField, FloatField, FileField, IntegerField
 from wtforms.validators import Required, Length, Regexp
 
 class UploadCar(Form):
@@ -13,5 +13,10 @@ class UploadCar(Form):
 	first_licensing_date = DateField(u'首次上牌日期', format="%Y-%m-%d", validators=[Required(),])
 	licensing_place = StringField(u'上牌地', validators=[Required(), Length(1,64),])
 	mileage = FloatField(u'里程数', validators=[Required(),])
+	type_of_gearbox = SelectField(u'变速箱类型', choices=[('auto', u'自动'), ('manual', u'手动'),], validators=[Required(),])
+	emission_standard = StringField(u'排放标准', validators=[Required(), Length(1,64),])
+	displacement = FloatField(u'排量', validators=[Required(),])
+	number_of_seats = IntegerField(u'座位数', validators=[Required(),])
+	age_of_car = IntegerField(u'车龄', validators=[Required(),])
 	image = FileField(u'上传图片', render_kw={'multiple':'multiple',},)
 	submit = SubmitField(u'提交')
